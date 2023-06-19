1923964043f041b5f8461c942fd0747cb214649d
Maxim <mxmglebov2005@gmail.com>
2023-06-19 11:30:17 +0300

    Added class Node

--- SR2/src/SR2.cpp
+++ SR2/src/SR2.cpp
@@ -2,12 +2,24 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <vector>
 using namespace std;
 
+class Node
+{
+public:
+	string tag;
+	string text;
+	vector<Node> children;
 
+	Node(const string& tag) : tag(tag) {}
 
-int main() {
+	void printTree() const;
+}
 
+main()
+{
+	
 
 
 	return 0;
