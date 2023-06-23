cdbdb393db978269e0ebda4b06ee478057c032e3
Maxim <mxmglebov2005@gmail.com>
2023-06-23 15:46:34 +0300

    Deleted unused arrays, fixed formatting

--- SR2/src/SR2.cpp
+++ SR2/src/SR2.cpp
@@ -12,12 +12,12 @@ public:
 	string text;
 	vector<Node> children;
 
-	Node(const string& tag) : tag(tag) {}
+	Node(const string &tag) : tag(tag) {}
 
-	void printTree(const string& tab) const;
-	void saveToXML(ofstream& file, const string& tab) const;
-	int counting(Node& root, string& tag);
-	void editXML(Node& root, string& tag, const string& value, int& count, vector<Node>& tags, vector<string*>& text_ptr, bool& worked);
+	void printTree(const string &tab) const;
+	void saveToXML(ofstream &file, const string &tab) const;
+	int counting(Node &root, string &tag);
+	void editXML(Node &root, string &tag, const string &value, int &count, vector<Node> &tags, bool &worked);
 };
 
 enum State
@@ -28,7 +28,7 @@ enum State
 	CloseTag
 };
 
-void parseXMLNode(ifstream& file, Node& node)
+void parseXMLNode(ifstream &file, Node &node)
 {
 	State state = State::Text;
 	string tag, text;
@@ -85,7 +85,7 @@ void parseXMLNode(ifstream& file, Node& node)
 				if (tag != node.tag)
 				{
 					cerr << "Error: invalid closing tag [" << tag
-						<< "] for opening tag [" << node.tag << "] !\n";
+						 << "] for opening tag [" << node.tag << "] !\n";
 					exit(1);
 				}
 				if (node.children.size() > 0)
@@ -103,7 +103,7 @@ void parseXMLNode(ifstream& file, Node& node)
 	}
 }
 
-Node parseXML(const string& fileName)
+Node parseXML(const string &fileName)
 {
 	ifstream file(fileName);
 	if (!file.is_open())
@@ -126,18 +126,18 @@ Node parseXML(const string& fileName)
 	}
 }
 
-void Node::printTree(const string& tab = "") const
+void Node::printTree(const string &tab = "") const
 {
 	string tmpTag = tag;
 	tmpTag[0] = toupper(tmpTag[0]);
-	cout << tab << tmpTag << ": " << text << endl;
-	for (const Node& child : children)
+	std::cout << tab << tmpTag << ": " << text << endl;
+	for (const Node &child : children)
 	{
 		child.printTree(tab + '\t');
 	}
 }
 
-void Node::saveToXML(ofstream& file, const string& tab) const
+void Node::saveToXML(ofstream &file, const string &tab) const
 {
 	file << tab << "<" << tag << ">";
 
@@ -146,17 +146,19 @@ void Node::saveToXML(ofstream& file, const string& tab) const
 
 	if (!children.empty())
 	{
-		file << endl << endl;
-		for (const Node& child : children)
+		file << endl
+			 << endl;
+		for (const Node &child : children)
 			child.saveToXML(file, tab + '\t');
 
 		file << tab;
 	}
 
-	file << "</" << tag << ">" << endl << endl;
+	file << "</" << tag << ">" << endl
+		 << endl;
 }
-
-void saveXML(const Node& root, const string& fileName)
+// merge into one function
+void saveXML(const Node &root, const string &fileName)
 {
 	ofstream file(fileName);
 	if (!file.is_open())
@@ -168,14 +170,14 @@ void saveXML(const Node& root, const string& fileName)
 	root.saveToXML(file, "");
 
 	file.close();
-	cout << "XML file saved as " << fileName << endl;
+	std::cout << "XML file saved as " << fileName << endl;
 }
 
-int Node::counting(Node& root, string& tag)
+int Node::counting(Node &root, string &tag)
 {
 	int count = 0;
 
-	for (Node& child : root.children)
+	for (Node &child : root.children)
 		count += child.counting(child, tag);
 
 	if (root.tag == tag || root.tag.empty())
@@ -184,35 +186,37 @@ int Node::counting(Node& root, string& tag)
 	return count;
 }
 
-void tagEdit(Node& root, vector<Node>& tags, const string& value, vector<string*>& text_ptr)
+void tagEdit(Node &root, vector<Node> &tags, const string &value)
 {
 	int choice;
 
-	cout << "Choose a tag to edit: ";
+	std::cout << "Choose a tag to edit: ";
 
 	for (int i = 0; i < tags.size(); i++)
-		cout << "\n\n" << i + 1 << ". <" << tags[i].tag << ">" << tags[i].text << "</" << tags[i].tag << ">";
+		std::cout << "\n\n"
+				  << i + 1 << ". <" << tags[i].tag << ">" << tags[i].text << "</" << tags[i].tag << ">";
 
-	cout << "\n\nYour choice: ";
+	std::cout << "\n\nYour choice: ";
 	cin >> choice;
 
-	*text_ptr[choice - 1] = value;
+	//*text_ptr[choice - 1] = value;
+	tags[choice - 1].text = value;
 
-	cout << "\nThe tag was updated successfully!";
+	std::cout
+		<< "\nThe tag was updated successfully!";
 
 	tags.clear();
-	text_ptr.clear();
 }
 
-void addTagToFile(Node& root, const string& tag, const string& value)
+void addTagToFile(Node &root, const string &tag, const string &value)
 {
 	Node newNode(tag);
 	newNode.text = value;
 	root.children.push_back(newNode);
-	cout << "\nThere is no such tag. The new tag <" << tag << "> was added." << endl;
+	std::cout << "\nThere is no such tag. The new tag <" << tag << "> was added." << endl;
 }
 
-void Node::editXML(Node& root, string& tag, const string& value, int& count, vector<Node>& tags, vector<string*>& text_ptr, bool& worked)
+void Node::editXML(Node &root, string &tag, const string &value, int &count, vector<Node> &tags, bool &worked)
 {
 	if (count >= 2)
 	{
@@ -221,36 +225,34 @@ void Node::editXML(Node& root, string& tag, const string& value, int& count, vec
 			if (root.tag == tag)
 			{
 				tags.push_back(root);
-				text_ptr.push_back(&root.text);
 			}
 
-			for (Node& child : root.children)
-				child.editXML(child, tag, value, count, tags, text_ptr, worked);
+			for (Node &child : root.children)
+				child.editXML(child, tag, value, count, tags, worked);
 		}
 	}
-	else
-		if (!root.children.empty())
-			for (Node& child : root.children)
-			{
-				worked = true;
-				child.editXML(child, tag, value, count, tags, text_ptr, worked);
-			}
-		else if (root.children.empty())
-			if (root.tag == tag)
-			{
-				worked = true;
-				root.text = value;
-			}
+	else if (!root.children.empty())
+		for (Node &child : root.children)
+		{
+			worked = true;
+			child.editXML(child, tag, value, count, tags, worked);
+		}
+	else if (root.children.empty())
+		if (root.tag == tag)
+		{
+			worked = true;
+			root.text = value;
+		}
 }
 
-void menu(Node& root, vector<Node>& tags, vector<string*>& text_ptr, static bool worked)
+void menu(Node &root, vector<Node> &tags, bool worked)
 {
 	int choice, returning;
 	string newTag, newValue;
 
 	do
 	{
-		cout << "1 - Edit tags\n2 - Save file\n3 - Compare two files\n4 - Print file\n5 - Exit\n\nYour choice: ";
+		std::cout << "1 - Edit tags\n2 - Save file\n3 - Compare two files\n4 - Print file\n5 - Exit\n\nYour choice: ";
 		cin >> choice;
 
 		switch (choice)
@@ -258,25 +260,24 @@ void menu(Node& root, vector<Node>& tags, vector<string*>& text_ptr, static bool
 		case 1:
 			root.printTree();
 
-			cout << "\n\nEnter a tag to edit: ";
+			std::cout << "\n\nEnter a tag to edit: ";
 			cin.ignore();
 			getline(cin, newTag);
-			cout << "\nEnter a new value for the tag: ";
+			std::cout << "\nEnter a new value for the tag: ";
 			getline(cin, newValue);
-			{
-				int count = root.counting(root, newTag);
-				if (count < 1)
-					addTagToFile(root, newTag, newValue);
-				else
-				{
-					root.editXML(root, newTag, newValue, count, tags, text_ptr, worked);
-					if (!worked)
-						tagEdit(root, tags, newValue, text_ptr);
-					else
-						cout << "\nThe tag was updated successfully!";
-				}
 
+			int count = root.counting(root, newTag);
+			if (count < 1)
+				addTagToFile(root, newTag, newValue);
+			else
+			{
+				root.editXML(root, newTag, newValue, count, tags, worked);
+				if (!worked)
+					tagEdit(root, tags, newValue);
+				else
+					std::cout << "\nThe tag was updated successfully!";
 			}
+
 			Sleep(3000);
 			system("cls");
 
@@ -290,14 +291,15 @@ void menu(Node& root, vector<Node>& tags, vector<string*>& text_ptr, static bool
 			break;
 		case 3:
 
-			//Here will be compare function
+			// Here will be compare function
 
 			break;
 		case 4:
 
 			root.printTree();
 
-			cout << endl << "Enter 0 to return to menu: ";
+			std::cout << endl
+					  << "Enter 0 to return to menu: ";
 			cin >> returning;
 
 			if (returning == 0)
@@ -313,11 +315,10 @@ void menu(Node& root, vector<Node>& tags, vector<string*>& text_ptr, static bool
 int main()
 {
 	Node root = parseXML("test.xml");
-	vector<string*> text_ptr;
 	vector<Node> tags;
 	bool worked = false;
 
-	menu(root, tags, text_ptr, worked);
+	menu(root, tags, worked);
 
 	return 0;
 }
